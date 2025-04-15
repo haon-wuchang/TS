55dcb2c6d5764bc8d6dc32530f9c6b6f55a99a3f
haon <haoony@naver.com>
2025-04-15 14:29:28 +0900

    4/15

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -2,41 +2,24 @@ import './App.css';
 import MenuList from './component/MenuList';
 import EmployeeList from './component/EmployeeList';
 import LunchList from './component/LunchList';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const menus = [
-    {
-      name: 'apple',
-      emoji: '🍎',
-      price: 100
-    },
-    {
-      name: 'banana',
-      emoji: '🍌',
-      source: '필리핀'
-    },
-    {
-      name: 'coconut',
-      emoji: '🥥',
-      price: 1000,
-    }
-  ];
-
-  const employees = [
-    { name: '홍길동', age: 25, department: '영업부', },
-    { name: '김민지', age: 30, department: '경영지원부', },
-    { name: '철수박', age: 55, department: '총무부', emoji: '👩‍🦳' }
-  ]
-
-  const foodList = [
-    { name: '피자', emoji: '🍕', price: 2200 },
-    { name: '샌드위치', emoji: '🥙', text: '존맛탱' },
-    { name: '초밥', emoji: '🍣', price: 1000 },
-    { name: '삼각김밥', emoji: '🍙', text: '모찌모찌 삼김' },
-    { name: '라멘', emoji: '🥣', price: 10000 }
-  ];
-
-
+  const [menus, setMenus] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [foodList, setFoodList] = useState([]);
+  
+  useEffect(()=>{
+    axios('data/ts.json')
+      .then((res)=>{
+        setMenus(res.data.menus);
+        setEmployees(res.data.employees);
+        setFoodList(res.data.foodList);
+      })
+      .catch(error => console.log(error));
+  },[]);
+  
   return (
     <div className="App">
       <h1>하이 타입스크립트</h1><hr />
